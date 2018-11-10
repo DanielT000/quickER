b0e909e81691b1c75287cd318585206741374c7f
DanielT000 <dantoh000@gmail.com>
2018-11-10 13:36:53 +0800

    Create greeting.h

---
+++ greeting.h
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+void greeting(){
+  cout<<"\n\n\n";
+	cout<<"    --------      |         |     ---------      --------     |  /      ---------     ---\n";
+	cout<<"   |        |     |         |         |         |             | /      |             |   |\n";
+	cout<<"   |        |     |         |         |         |             |/       |              ---\n";
+	cout<<"   |        |     |         |         |         |             |\\        ---------    |\\\n";
+	cout<<"   |        |     |         |         |         |             | \\      |             | \\\n";
+	cout<<"    -----\\--       ---------      ---------      --------     |  \\      ---------    |  \\\n";
+	cout<<"          \\";
+	cout<<"\n   Welcome to quickER. quickER is a program that saves lives by making waiting times for treatments in Emergency Rooms quickER.";
+  }

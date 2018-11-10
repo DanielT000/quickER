681de14e8c59261acc377fdfa976206b6ad88012
DanielT000 <dantoh000@gmail.com>
2018-11-10 17:38:17 +0800

    Add files via upload

--- distance.h
+++ distance.h
@@ -1,10 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef pair<double,double> dd;
-dd ERlocations[] = {dd(1.337665316,103.942829562),dd(1.306332108 103.841496634),dd(1.306332108 103.841496634),dd(1.4246346 103.8382084),dd(1.3342547 103.74493),dd(1.2903821718 103.776363561),dd(1.2903821718 103.776363561),dd(1.2738655712 103.833946664),dd(1.3955377 103.8935289),dd(1.3217763 103.8462335)}
-const double radius = 6378137;
+dd ERlocations[] = {make_pair(1.337665316,103.942829562),make_pair(1.306332108,103.841496634),make_pair(1.306332108,103.841496634),make_pair(1.4246346,103.8382084)
+,make_pair(1.3342547,103.74493),make_pair(1.2903821718,103.776363561),make_pair(1.2903821718,103.776363561),make_pair(1.2738655712,103.833946664),make_pair(1.3955377,103.8935289),make_pair(1.3217763,103.8462335)};
+const double radius = 6378.137;
+const double pi = 3.1415926535897932384626;
+const double travelspeed = 0.861111114;
+double tiem(double dist){
+    return dist/travelspeed;
+}
 double distance(double x1, double y1, double x2, double y2){
-    return 2*r*arcsin(sqrt(sin((x2-x1)/2.00)*sin((x2-x1)/2.00) + cos(x1)*cos(x2)*sin((y2-y1)/2)*sin((y2-y1)/2));
+    return 1.366124295*2*radius*asin(sqrt(sin((x2-x1)*pi/360)*sin((x2-x1)*pi/360) + cos(x1*pi/180)*cos(x2*pi/180)*sin((y2-y1)*pi/360)*sin((y2-y1)*pi/360)));
 }
 double distance(dd a1, dd a2){
     return distance(a1.first,a1.second,a2.first,a2.second);
--- greeting.h
+++ greeting.h
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 void greeting(){
-  cout<<"\n\n\n";
-	cout<<"    --------      |         |     ---------      --------     |  /      ---------     ---\n";
-	cout<<"   |        |     |         |         |         |             | /      |             |   |\n";
-	cout<<"   |        |     |         |         |         |             |/       |              ---\n";
-	cout<<"   |        |     |         |         |         |             |\\        ---------    |\\\n";
-	cout<<"   |        |     |         |         |         |             | \\      |             | \\\n";
-	cout<<"    -----\\--       ---------      ---------      --------     |  \\      ---------    |  \\\n";
-	cout<<"          \\";
-	cout<<"\n   Welcome to quickER. quickER is a program that saves lives by making waiting times for treatments in Emergency Rooms quickER.";
+    cout << "               _        _     ______  _____  \n";
+    cout << "              (_)      | |   |  ____||  __ \\ \n";
+    cout << "  __ _  _   _  _   ___ | | __| |__   | |__) |\n";
+    cout << " / _` || | | || | / __|| |/ /|  __|  |  _  / \n";
+    cout << "| (_| || |_| || || (__ |   < | |____ | | \\ \\ \n";
+    cout << " \\__, | \\__,_||_| \\___||_|\\_\\|______||_|  \\_\\\n";
+    cout << "    | |                                      \n";
+    cout << "    |_|                                      \n";
+	cout<<"\nWelcome to quickER. quickER is a program that saves lives by making waiting times for treatments in Emergency Rooms quickER.";
   }

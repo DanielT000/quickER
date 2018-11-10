77e46e209611a3f0a04771311fb870c70600bd3a
DanielT000 <dantoh000@gmail.com>
2018-11-10 14:10:10 +0800

    Update distance.h

--- distance.h
+++ distance.h
@@ -3,7 +3,7 @@ using namespace std;
 typedef pair<double,double> dd;
 const double radius = 6378137;
 double distance(double x1, double y1, double x2, double y2){
-    return 2*r*arcsin(sqrt(hav()))
+    return 2*r*arcsin(sqrt(sin()))
 }
 double distance(dd a1, dd a2){
     return distance(a1.first,a1.second,a2.first,a2.second);

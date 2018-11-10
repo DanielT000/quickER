f901039bf7f68319680cdd6186eb39158312c36b
DanielT000 <dantoh000@gmail.com>
2018-11-10 14:06:59 +0800

    Update distance.h

--- distance.h
+++ distance.h
@@ -1,7 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-void hello(){
-    printf("hello\n");
-    return;
+typedef pair<double,double> dd;
+const double radius = 6378137;
+double distance(double x1, double y1, double x2, double y2){
+    return 2*r*arcsin(sqrt(hav()))
+}
+double distance(dd a1, dd a2){
+    return distance(a1.first,a1.second,a2.first,a2.second);
 }
 
